b88909498a7fcc9a60ac3ad86e6aadb1d6243d6b
prifio <tolya_ash@mail.ru>
2016-03-19 15:33:48 +0300

    Add example
    Add example drawing green screen with text "Hello, world!". PictureBox
    size = form client size.

--- HuntTheWumpus/HuntTheWumpus/Form1.cs
+++ HuntTheWumpus/HuntTheWumpus/Form1.cs
@@ -16,19 +16,20 @@ namespace HuntTheWumpus
         {
             InitializeComponent();
             pictureBox1.Paint += DrawForm;
+            pictureBox1.Width = this.ClientSize.Width;
+            pictureBox1.Height = this.ClientSize.Height;
             //this.KeyDown += ??.KeyDown; KeyDown(object sender, KeyEventArgs e)
             //this.pictureBox1.MouseDown += ??.MouseDown; MouseDown(object sender, MouseEventArgs e) 
             //this.pictureBox1.MouseUp += ??.MouseUp; MouseUp(object sender, MouseEventArgs e)
             //this.pictureBox1.MouseMove += ??.MouseMove; MouseMove(object sender, MouseEventArgs e)
-            this.FormBorderStyle = FormBorderStyle.None;//If we want form without border
-
+            //this.FormBorderStyle = FormBorderStyle.None;//If we want form without border
         }
 
         public void DrawForm(System.Object sender, System.Windows.Forms.PaintEventArgs e)
         {
-            var bmp = new System.Drawing.Bitmap(this.Width, this.Height);
+            var bmp = new System.Drawing.Bitmap(this.ClientSize.Width, this.ClientSize.Height);
             var g = System.Drawing.Graphics.FromImage(bmp);
-            //??.GetScreen(g);
+            Program.GetScreen(g);
             e.Graphics.DrawImage(bmp, 0, 0);
         }
 
--- HuntTheWumpus/HuntTheWumpus/Program.cs
+++ HuntTheWumpus/HuntTheWumpus/Program.cs
@@ -16,7 +16,7 @@ namespace HuntTheWumpus
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            System.Threading.Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
+            System.Threading.Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;//double.Parse(str) use format a.b
             var MainForm = new Form1();
             MainForm.Show();
             MainForm.DrawAll();
@@ -29,6 +29,13 @@ namespace HuntTheWumpus
                 //Apdate(ticktim.ElapsedMilliseconds);
                 ticktim.Restart();
             }
+        }
+
+        public static void GetScreen(System.Drawing.Graphics g)
+        {
+            g.Clear(System.Drawing.Color.Green);
+            var fn = new System.Drawing.Font("Arial", 20);
+            g.DrawString("Hello, world!", fn, System.Drawing.Brushes.Red, 20, 30);
         }
     }
 }
